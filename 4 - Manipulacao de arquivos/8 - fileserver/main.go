2e425cdcea223d18eb74e265eb12530494a952c6
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-19 13:39:30 -0300

    file server

---
+++ 4 - Manipulacao de arquivos/8 - fileserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./public"))
+
+	mux.Handle("/", fileServer)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
+}
