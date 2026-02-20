2a8c7124b0bd6b3739aade99f97b03617aa5571c
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-20 11:26:25 -0300

    pacotes

---
+++ 8 - pacotes/1/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"pacotes1/math"
+)
+
+func main() {
+
+	fmt.Println("Hello, World")
+
+	resposta := math.Math{A: 1, B: 2}
+	fmt.Println(resposta)
+}
---
+++ 8 - pacotes/1/go.mod
@@ -0,0 +1,3 @@
+module pacotes1
+
+go 1.25.7
