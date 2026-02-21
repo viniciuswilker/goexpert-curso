58d7d8487f502e81d27dbed1fb362581c18b0202
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-21 13:11:59 -0300

    final teste

--- 10 - apis/1/internal/infra/database/product_db_test.go
+++ 10 - apis/1/internal/infra/database/product_db_test.go
@@ -80,3 +80,41 @@ func TestFindProductByID(t *testing.T) {
 	assert.Equal(t, "Product 1", product.Name)
 
 }
+
+func TestUpdateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+
+	productDB := NewProduct(db)
+	product.Name = "Product 2"
+	err = productDB.Update(product)
+	assert.NoError(t, err)
+
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 2", product.Name)
+}
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	_, err = productDB.FindByID(product.ID.String())
+	assert.Error(t, err)
+}
