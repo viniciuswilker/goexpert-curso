05995398066f9947bd7e1116e296beaaf37953b3
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-21 15:15:16 -0300

    rotas de crud de pedidos

--- 10 - apis/1/cmd/server/main.go
+++ 10 - apis/1/cmd/server/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"1-api/configs"
+	"1-api/internal/entity"
+	"1-api/internal/infra/database"
+	"1-api/internal/webserver/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, _ := configs.CarregarConfig(".")
-	println(config.DBDriver)
+	_, err := configs.CarregarConfig(".")
+	if err != nil {
+		panic(err)
+	}
 
+	db, err := gorm.Open(sqlite.Open("teste.db"))
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/products", productHandler.GetProducts)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	http.ListenAndServe(":8000", r)
 }
--- 10 - apis/1/go.mod
+++ 10 - apis/1/go.mod
@@ -3,6 +3,7 @@ module 1-api
 go 1.25.7
 
 require (
+	github.com/go-chi/chi v1.5.1
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.6.0
 	github.com/spf13/viper v1.21.0
